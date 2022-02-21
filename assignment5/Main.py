f4b7bd2bf773fe64e7671c75efce2164141dd009
Maryclare <mleonar8@nd.edu>
2022-02-21 14:33:25 -0500

    DataSet ~  Duck

--- assignment5/Main.py
+++ assignment5/Main.py
@@ -1,7 +1,19 @@
 from abc import ABC, abstractmethod
-#from sort import *
-#from filter import *
-#from average import *
+from Sorter import *
+from Filter import *
+from Averager import *
+
+from random import shuffle, random
+
+#random generator
+squares = list(n * n for n in range(10000))
+shuffle(squares)
+# Test: print(squares)
+evens = list(n * 2 for n in range(500))
+shuffle(evens)
+# Test: print(evens)
+floats = list (random() for n in range(200))
+# Test: print (floats)
 
 class Sorter(ABC):
     def _init_(self):
@@ -18,7 +30,15 @@ class Averager(ABC):
 class DataSet(ABC):
     def _init_(self,Sorter,Filterer,Averager):
         super()._init()
-        self.sort = Sorteri
+        self.sort = Sorter
         self.filter = Filterer
         self.average = Averager
+    @abstractmethod
+    def sort(self):
+        self.sort.sort()
+    def filter(self):
+        self.filter.filter()
+    def average(self):
+        self.average.average()
+
     
