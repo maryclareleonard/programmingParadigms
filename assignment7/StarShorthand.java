dae91cfee1488a85c3b448ca2211c1dcbb0843a1
Maryclare <mleonar8@nd.edu>
2022-03-21 15:05:20 -0400

    debug last piece

--- assignment7/StarShorthand.java
+++ assignment7/StarShorthand.java
@@ -13,25 +13,41 @@ public class StarShorthand {
         System.out.println(toStarShorthand("")); 
     }
     public static String toStarShorthand(String s){
+
         int count = 0;
         String first;
         String second;
         String third;
         int secondPos;
-        for (int i=0; i < s.length()-1; i++) {
-            if ( s.charAt(i) == s.charAt(i+1)) 
-                count++;
+
+        for (int i=0; i <= s.length()-2; i++) {
+            if ( s.charAt(i) == s.charAt(i+1)) {
+                count = count + 1;
+            }
             else {
                 if (count > 0) {
                     secondPos = i-count;
-                    System.out.println(secondPos);
                     first = s.substring(0,secondPos);
+                    //System.out.println(Integer.toString(count+1));
                     second = s.charAt(i) + "*" + Integer.toString(count+1);
+                    //System.out.println(second);
                     third = s.substring(i+1,s.length()-1);
-                    System.out.println(first + second + third);
+                    //System.out.println(third);
+                    s = first + second + third; 
+                    //System.out.println(s);
                     count = 0;
                 }
             }
+        }
+        //to get repetition at end
+        if (count > 0) {
+            int i = s.length()-2;
+            secondPos = i;
+            first = s.substring(0,secondPos);
+            second = s.charAt(i) + "*" + Integer.toString(count+2);
+            third = s.substring(i+1,s.length()-1);
+            s = first + second + third; 
+            //System.out.println(m); 
         }
         return s;
     }
