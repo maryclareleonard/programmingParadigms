5a7138de88ec5d1c4050ea4e96be1c8fadebe41a
Maryclare <mleonar8@nd.edu>
2022-03-21 14:15:21 -0400

    first catch

--- assignment7/StarShorthand.java
+++ assignment7/StarShorthand.java
@@ -14,10 +14,24 @@ public class StarShorthand {
     }
     public static String toStarShorthand(String s){
         int count = 0;
+        String first;
+        String second;
+        String third;
+        int secondPos;
         for (int i=0; i < s.length()-1; i++) {
             if ( s.charAt(i) == s.charAt(i+1)) 
                 count++;
-            System.out.println(count);
+            else {
+                if (count > 0) {
+                    secondPos = i-count;
+                    System.out.println(secondPos);
+                    first = s.substring(0,secondPos);
+                    second = s.charAt(i) + "*" + Integer.toString(count+1);
+                    third = s.substring(i+1,s.length()-1);
+                    System.out.println(first + second + third);
+                    count = 0;
+                }
+            }
         }
         return s;
     }
