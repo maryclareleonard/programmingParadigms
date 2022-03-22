4a88d9fab53189d10d594a80b467f73a787091ab
Maryclare <mleonar8@nd.edu>
2022-03-21 20:55:58 -0400

    part one done

--- assignment7/StarShorthand.java
+++ assignment7/StarShorthand.java
@@ -20,7 +20,7 @@ public class StarShorthand {
         String third;
         int secondPos;
 
-        for (int i=0; i <= s.length()-2; i++) {
+        for (int i=0; i < s.length()-1; i++) {
             if ( s.charAt(i) == s.charAt(i+1)) {
                 count = count + 1;
             }
@@ -28,26 +28,24 @@ public class StarShorthand {
                 if (count > 0) {
                     secondPos = i-count;
                     first = s.substring(0,secondPos);
-                    //System.out.println(Integer.toString(count+1));
                     second = s.charAt(i) + "*" + Integer.toString(count+1);
-                    //System.out.println(second);
-                    third = s.substring(i+1,s.length()-1);
-                    //System.out.println(third);
+                    third = s.substring(count+2,s.length());
                     s = first + second + third; 
-                    //System.out.println(s);
+                    System.out.println("s: " + s);
                     count = 0;
                 }
             }
         }
-        //to get repetition at end
+
         if (count > 0) {
-            int i = s.length()-2;
-            secondPos = i;
-            first = s.substring(0,secondPos);
-            second = s.charAt(i) + "*" + Integer.toString(count+2);
-            third = s.substring(i+1,s.length()-1);
-            s = first + second + third; 
-            //System.out.println(m); 
+                System.out.println("here");
+                System.out.println(count);
+                secondPos = s.length()-(count+1);
+                first = s.substring(0,secondPos);
+                System.out.println(first);
+                second = s.charAt(secondPos) + "*" + Integer.toString(count+1);
+                System.out.println(second);
+                s = first + second; 
         }
         return s;
     }
