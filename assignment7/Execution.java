ced034a07396720175e99da54539e169a9500e25
Maryclare <mleonar8@nd.edu>
2022-03-23 00:35:47 -0400

    original main coverted to race so a SIMPLE main

---
+++ assignment7/Execution.java
@@ -0,0 +1,28 @@
+public class Execution {
+    private Strategy strategy;
+
+    public Execution (Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    //ability to change strategy
+    public void setStrategy(Strategy newStrategy) {
+        this.strategy = newStrategy;
+    }
+    
+    public int executeStrategy(Horse h) {
+        h.setPosition(strategy.runRace(h));
+        System.out.println(h.getName() + " current position: " + h.getPosition());
+        System.out.println();
+
+        if (h.getPosition() >= 10) {
+            h.setDone(true);
+            return 1;
+        }
+        else {
+            return 0;
+        }
+
+    }
+
+}
--- assignment7/Race.java
+++ assignment7/Race.java
@@ -1,23 +1,74 @@
-public class Race {
-    private Strategy strategy;
+public class Race{ 
+    //add horses
 
-    public Race (Strategy strategy) {
-        this.strategy = strategy;
-    }
+    int dummy = -1;
+    int count = 0;
 
-    public int executeStrategy(Horse h) {
-        h.setPosition(strategy.runRace(h));
-        System.out.println(h.getName() + " current position: " + h.getPosition());
+    public Race() {
+        //add horses with strategies
+        Execution execution1 = new Execution(new SlowStart());
+        Horse h1 = new Horse("Ayman",2,"Green");
+
+        Execution execution2 = new Execution(new EarlySprint());
+        Horse h2 = new Horse("Becky",2,"Blue");
+            
+        Execution execution3 = new Execution(new SteadyRun());
+        Horse h3 = new Horse("John",3,"Pink");
+
+        Execution execution4 = new Execution(new SlowStart());
+        Horse h4 = new Horse("Ben",3,"Red");
+            
+        Execution execution5 = new Execution(new EarlySprint());
+        Horse h5 = new Horse("Natalie",3,"Yellow");
+        
+        //run race
+        while ( !(h1.getDone() && h2.getDone() && h3.getDone() && h4.getDone() && h5.getDone() )) {
+            System.out.println();
+            System.out.println("--------------");
+            System.out.println(count + " Seconds");
+            count = count + 1;
+            int done1 = execution1.executeStrategy(h1);
+            int done2 = execution2.executeStrategy(h2);
+            int done3 = execution3.executeStrategy(h3);
+            int done4 = execution4.executeStrategy(h4);
+            int done5 = execution5.executeStrategy(h5);
+
+            if (dummy == -1) {
+                if (done1 == 1) {
+                    h1.setWinner(true);
+                    dummy = 0;
+                } else if (done2 == 1) {
+                    h2.setWinner(true);
+                    dummy = 0;
+                } else if (done3 == 1) {
+                    h3.setWinner(true);
+                    dummy = 0;
+                } else if (done4 == 1) {
+                    h4.setWinner(true);
+                    dummy = 0;
+                } else if (done5 == 1) {
+                    h5.setWinner(true);
+                    dummy = 0;
+                }
+            }
+            System.out.println("--------------");
+            System.out.println();
+        }
+
+        // announce winner
         System.out.println();
-
-        if (h.getPosition() >= 10) {
-            h.setDone(true);
-            return 1;
+        System.out.println("The race is complete");
+        if(h1.getWinner()) {
+            System.out.println(h1.getName() + " in " + h1.getColor() + " won!");
+        } else if(h2.getWinner()){
+            System.out.println(h2.getName() + " in " + h2.getColor() + " won!");
+        } else if(h3.getWinner()){
+            System.out.println(h3.getName() + " in " + h3.getColor() + " won!");
+        } else if(h4.getWinner()){
+            System.out.println(h4.getName() + " in " + h4.getColor() + " won!");
+        } else if(h5.getWinner()){
+            System.out.println(h5.getName() + " in " + h5.getColor() + " won!");
         }
-        else {
-            return 0;
-        }
-
+        System.out.println();
     }
-
 }
