7cf05217bb3d5de8d7b6eb27eb9c63d182866eb3
Maryclare <mleonar8@nd.edu>
2022-03-28 11:09:22 -0400

    armstrong working

--- assignment8/number1/Armstrong.java
+++ assignment8/number1/Armstrong.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 public class Armstrong {
     int start;
     int end;
@@ -10,20 +12,47 @@ public class Armstrong {
     }
 
     int numLength;
+    int denom;
+    int thDigit;
+    int currSum;
+    int indexArmstrongs = 0;
 
-    public int[] getArmstrongs() {
+
+    public void getArmstrongs() {
+        
         for (int i = this.start; i <= this.end; i++) {
-            System.out.println();
+            currSum = 0;
+            //System.out.println();
             numLength = String.valueOf(i).length();
-            System.out.println("Length of num: " + numLength);
+            //System.out.println("Length of num: " + numLength);
             for (int x = 0; x < numLength; x++) {
-                System.out.println("Num's " + x + "th digit: " + i%(10*(x+1)) );
+                if (x == 0) { denom = 1;}
+                else        { denom = (int)(Math.pow(10,x)); 
+                    //System.out.println("Denom: " + denom);
+                }
+                thDigit = (i/(denom))%10; 
+                //System.out.println("Num's " + x + "th digit: " + thDigit);
+                currSum = addToSum(currSum, thDigit, numLength);
             }
-            System.out.println();
+            if (currSum == i) {
+                armstrongNumbers[indexArmstrongs] = i;
+                indexArmstrongs++;
+            }
+            //System.out.println("Sum: " + currSum);
         }
-        armstrongNumbers[0] = 140;
-        armstrongNumbers[1] = 200;
-        return armstrongNumbers;
+        System.out.println("Here are the Armstrongs: ");
+        for (int pos = 0; pos < indexArmstrongs; pos++ ){
+            System.out.println(armstrongNumbers[pos]);
+        }
+        //return armstrongNumbers;
     }
+
+    public int addToSum(int currSum, int digit, int length) {
+        //System.out.println("Digit: " + digit);
+        //System.out.println("Length: " + length);
+        //System.out.println("CurrSum: " + currSum ); 
+        return currSum + (int)(Math.pow(digit,length));
+    }
+    
 
 }
