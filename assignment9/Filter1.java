2944056b1e933f236cc4b60cf7c5a6c27313e018
Maryclare <mleonar8@nd.edu>
2022-04-02 14:44:58 -0400

    reading file in Filter 1

--- assignment9/Filter1.java
+++ assignment9/Filter1.java
@@ -1,9 +1,30 @@
+import java.io.BufferedReader; //import class to handle errors
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import java.util.stream.Collectors; //implement various reduction operations
+
 public class Filter1 implements Filter{
 
     public Filter1() {}
     //must implement the inherited abstract method Runnable.run()
     @Override
     public void run() {
+        try {
+            //buffered reader is synchronized (thread safe) - scanner is not
+            BufferedReader input = new BufferedReader(new FileReader("file.txt"));
+            System.out.println(readAllLinesWithStream(input));
 
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An error occured: " + e);
+            e.printStackTrace();
+        }
+        
+    }
+
+    public String readAllLinesWithStream(BufferedReader reader) {
+        System.out.println("here");
+        return reader.lines().collect(Collectors.joining(System.lineSeparator()));
     }
 }
