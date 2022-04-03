4f998aee585649ea8b10bbb2ebc81a2584d28cc4
Maryclare <mleonar8@nd.edu>
2022-04-03 11:33:14 -0400

    filter 1: blockingqueue cap set

--- assignment9/Filter1.java
+++ assignment9/Filter1.java
@@ -1,10 +1,13 @@
 import java.io.BufferedReader; //import class to handle errors
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-
-import java.util.stream.Collectors; //implement various reduction operations
+import java.io.IOException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Filter1 implements Filter{
+
+    BlockingQueue<String> myQueue = new LinkedBlockingQueue<> (10);
 
     public Filter1() {}
     //must implement the inherited abstract method Runnable.run()
@@ -12,19 +15,31 @@ public class Filter1 implements Filter{
     public void run() {
         try {
             //buffered reader is synchronized (thread safe) - scanner is not
-            BufferedReader input = new BufferedReader(new FileReader("file.txt"));
-            System.out.println(readAllLinesWithStream(input));
-
-        }
-        catch (FileNotFoundException e) {
+            BufferedReader reader = new BufferedReader(new FileReader("fileLong.txt"));
+            try {
+                String line = reader.readLine();
+                while (line != null) { 
+                    System.out.println(line);
+                    try {
+                        myQueue.put(line); //add line to queue if space
+                    } catch (InterruptedException ie0 ){
+                        System.out.println("An error occured: " + ie0);
+                        ie0.printStackTrace(); 
+                    }
+                    line = reader.readLine();
+                }
+            } catch (IOException i) {
+                System.out.println("An error occured: " + i);
+                i.printStackTrace();
+            }
+            try { reader.close(); } catch (IOException i2) {
+                System.out.println("An error occured: " + i2);
+                i2.printStackTrace(); 
+            }
+        } catch (FileNotFoundException e) {
             System.out.println("An error occured: " + e);
             e.printStackTrace();
         }
-        
     }
 
-    public String readAllLinesWithStream(BufferedReader reader) {
-        System.out.println("here");
-        return reader.lines().collect(Collectors.joining(System.lineSeparator()));
-    }
 }
--- assignment9/Main.java
+++ assignment9/Main.java
@@ -1,8 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+
+        //create first filter
         Filter filter1 = new Filter1(); //create a instance of first filter
         Runnable runnable1 = filter1; //create a runnable instance(
         Thread thread = new Thread(runnable1); //pop it into a thread
         thread.start(); //start the thread
+
+        //create second filter
+        Filter filter2 = new Filter2(filter1); //must know of first 1, constructor takes arg
+        Runnable runnable2 = filter2;
+        Thread thread2 = new Thread(runnable2);
+        thread2.start();
+
     }
 }
